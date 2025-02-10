71ebb113f65055829e49156b763905b71bce7b98
ilhame sadik <sadikilham2002@gmail.com>
2025-02-10 13:40:04 +0100

    navbar

---
+++ app/(landingpage)/components/navbar.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import { ModeToggle } from "@/components/modetoggle";
+import { cn } from "@/lib/utils";
+import Link from "next/link";  
+import { usePathname } from "next/navigation";
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+
+const NavLinks = [
+  { label: "Home", href: "/" },
+  { label: "Share your story", href: "/story" },
+  { label: "Browse stories", href: "/stories" },
+];
+
+const Navbar = () => {  
+  const pathname = usePathname();
+
+  return (
+    <div className="py-5 fixed z-50 w-full bg-background">
+      <div className="flex max-w-7xl mx-auto px-5 items-center justify-between">
+        <img
+          src="/logo-black.svg"
+          className="dark:hidden"
+          width={100}
+          height={100}
+          alt="logo"
+        />
+        <img
+          src="/logo-white.svg"
+          className="hidden dark:block"
+          width={100}
+          height={100}
+          alt="logo"
+        />
+        
+        {/* Desktop Navigation */}
+        <div className="md:flex hidden gap-x-3">
+          {NavLinks.map(({ href, label }) => (
+            <Link
+              key={href}
+              href={href}
+              className={cn(
+                "text-sm text-muted-foreground hover:text-primary transition-colors",
+                pathname === href && "text-primary font-semibold"
+              )}
+            >
+              {label}
+            </Link>
+          ))}
+        </div>
+        
+        <div className="flex items-center gap-x-2">
+          <ModeToggle />
+          
+          {/* Mobile Navigation */}
+          <div className="md:hidden flex">
+            <Sheet>
+            <SheetTrigger>
+            <div  style={{border:" 1px solid #e5e7eb", margin: "0px", padding:" 7px", borderRadius:" 6px"}}>
+            <Menu size={20} />
+            </div>
+            </SheetTrigger>
+              <SheetContent side="left">
+                <div className="flex flex-col gap-4 mt-5">
+                <img
+          src="/logo-black.svg"
+          className="dark:hidden"
+          width={100}
+          height={100}
+          alt="logo"
+        />
+        <img
+          src="/logo-white.svg"
+          className="hidden dark:block"
+          width={100}
+          height={100}
+          alt="logo"
+        />
+                  {NavLinks.map(({ href, label }) => (
+                    <Link
+                      key={href}
+                      href={href}
+                      className={cn(
+                        "text-lg text-muted-foreground hover:text-primary transition-colors",
+                        pathname === href && "text-primary font-semibold"
+                      )}
+                    >
+                      {label}
+                    </Link>
+                  ))}
+                </div>
+              </SheetContent>
+            </Sheet>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- app/(landingpage)/layout.tsx
+++ app/(landingpage)/layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './components/navbar'
  
 const landingpageLayout= ({children}:{children:React.ReactNode}) => {
   return (
     <div> 
-
+<Navbar/>
     <main>
         {children}
     </main>
